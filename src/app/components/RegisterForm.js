9576337cf921eeb2365a5f9cac81bb32a9b35476
CESAR NAVARRO <34008442+seonatic@users.noreply.github.com>
2018-06-10 19:38:43 +0200

    Register Form

---
+++ src/app/components/RegisterForm.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+const styles = theme => ({
+  margin: {
+    margin: theme.spacing.unit,
+  },
+});
+
+function InputWithIcon(props) {
+  const { classes } = props;
+
+  return (
+    <div>
+      <FormControl className={classes.margin}>
+        <InputLabel htmlFor="input-with-icon-adornment">Username</InputLabel>
+        <Input
+          id="Username-field"
+          startAdornment={
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          }
+        />
+      </FormControl>
+      <TextField className={classes.margin}
+        id="Password-field"
+        label="Password"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          ),
+        }}
+      />
+       <TextField className={classes.margin}
+        id="Password-field2"
+        label="Repeat Password"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AccountCircle />
+            </InputAdornment>
+          ),
+        }}
+      />
+   
+    </div>
+  );
+}
+
+InputWithIcon.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(InputWithIcon);
