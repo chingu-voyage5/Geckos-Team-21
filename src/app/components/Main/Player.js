2a3e601101b38efd073af6e48b1b8c6db4a32100
Misho <stevovic@gmail.com>
2018-06-12 05:29:40 +0200

    CSS cleaning

--- src/app/components/Main/Player.js
+++ src/app/components/Main/Player.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import YouTubePlayer from 'react-player/lib/players/YouTube'
+import YouTubePlayer from 'react-player/lib/players/YouTube';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = theme => ({
   playerWrapper: {
     position: 'relative',
-    paddingTop: '56.25%',
-    flex: '2 0 70%',
+    paddingTop: '56.25%'
   },
   reactPlayer: {
-    maxHeight: '750px',
     position: 'absolute',
     top: 0,
-    left: 0,
+    left: 0
   }
 });
 
@@ -28,7 +26,7 @@ const Player = ({ classes }) => (
           }
         }
       }}
-      url='https://www.youtube.com/watch?v=twcOr043i4k'
+      url="https://www.youtube.com/watch?v=jtgA0jvhp2A"
       controls
       width="100%"
       height="100%"
@@ -41,4 +39,3 @@ Player.propTypes = {
 };
 
 export default withStyles(styles)(Player);
-
