9396d8341126fb38576e1463d05afde78c6cef57
CESAR NAVARRO <34008442+seonatic@users.noreply.github.com>
2018-06-15 12:08:26 +0200

    Layout updates with Bootstrap

--- src/app/components/RegisterPage.js
+++ src/app/components/RegisterPage.js
@@ -1,53 +1,89 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import RegisterForm from './RegisterForm';
-import Submit from './Submit';
-
-  class Register extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        username: '',
-        password: ''
-      };
-      this.onChange = this.onChange.bind(this);
-      this.onSubmit = this.onSubmit.bind(this); 
-    }
- 
-
-onSubmit(e){
-
-  e.preventDefault();
-
-  const userData = {
-    email: this.state.username,
-    password: this.state.password
-  };
-}
-
-onChange(e){
-
-  this.setState({ [e.target.name]: e.target.value });
-
-}
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 
-
-    render() {
-      return (
-
-<div>
-
-< RegisterForm />
-< Submit />
-<p>Do you have an account? <Link to="/">Login</Link></p>
-
-</div>
-      );
-    }
+class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
+  onSubmit(e) {
+    e.preventDefault();
+    console.log("works");
+    const userData = {
+      email: this.state.username,
+      password: this.state.password
+    };
+  }
 
-  
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <div class="register">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-8 m-auto">
+                <h1 class="display-4 text-center">Sign Up</h1>
+                <p class="lead text-center">Create an account</p>
+                <form action="create-profile.html">
+                  <div class="form-group">
+                    <input
+                      type="text"
+                      class="form-control form-control-lg"
+                      placeholder="Name"
+                      name="name"
+                      required
+                    />
+                  </div>
+                  <div class="form-group">
+                    <input
+                      type="email"
+                      class="form-control form-control-lg"
+                      placeholder="Email Address"
+                      name="email"
+                    />
+                    <small className="form-text text-muted" />
+                  </div>
+                  <div class="form-group">
+                    <input
+                      type="password"
+                      class="form-control form-control-lg"
+                      placeholder="Password"
+                      name="password"
+                    />
+                  </div>
+                  <div class="form-group">
+                    <input
+                      type="password"
+                      class="form-control form-control-lg"
+                      placeholder="Confirm Password"
+                      name="password2"
+                    />
+                  </div>
+                  <input type="submit" class="btn btn-info btn-block mt-4" />
+                </form>
+
+                <p>
+                  Do you have an account? <Link to="/">Login</Link>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Register;
+
